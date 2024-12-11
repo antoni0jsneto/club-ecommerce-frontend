d7a688c95b0d2ebcb755cd4432384e70ec2ba360
antoni0neto <antonio.silva6@yahoo.com.br>
2024-12-11 11:29:14 -0300

    chore: add firebase configuration

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB58fdfyA_42_Rtnk0l_pBebTx3qAibAPk",
+    authDomain: "club-ecommerce-d6ae2.firebaseapp.com",
+    projectId: "club-ecommerce-d6ae2",
+    storageBucket: "club-ecommerce-d6ae2.firebasestorage.app",
+    messagingSenderId: "800254634526",
+    appId: "1:800254634526:web:746b3a26622a240302dc0f",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
