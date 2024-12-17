fe98ae8a51c7d7335c445f4773d188f57feffc4e
antoni0neto <antonio.silva6@yahoo.com.br>
2024-12-17 12:12:32 -0300

    feat: add Product Item Component

--- src/components/category-overview/category-overview.component.tsx
+++ src/components/category-overview/category-overview.component.tsx
@@ -10,6 +10,9 @@ import {
     ProductsContainer,
 } from "./category-overview.styles";
 
+// Components
+import ProductItem from "../product-item/product-item.component";
+
 interface CategoryOverviewProps {
     category: Category;
 }
@@ -21,7 +24,11 @@ const CategoryOverview: FunctionComponent<CategoryOverviewProps> = ({
         <CategoryContainer>
             <CategoryTitle>{category.displayName}</CategoryTitle>
 
-            <ProductsContainer></ProductsContainer>
+            <ProductsContainer>
+                {category.products.slice(0, 4).map((product) => (
+                    <ProductItem key={product.id} product={product} />
+                ))}
+            </ProductsContainer>
         </CategoryContainer>
     );
 };
